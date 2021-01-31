c5863bf54056b9b4ebe5a0680d9c3f609b079111
Charlotte THOMAS <contact@nwa2coco.fr>
2021-01-31 10:31:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Welcome to ocaml-toplevel-loader 👋
+![Version](https://img.shields.io/badge/version-0.0.1-blue.svg?cacheSeconds=2592000)
+![Prerequisite](https://img.shields.io/badge/vscode-%5E1.51.0-blue.svg)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](#)
+[![Twitter: coco33920](https://img.shields.io/twitter/follow/coco33920.svg?style=social)](https://twitter.com/coco33920)
+
+> A simple and lazy file-to-toplevel loader for OCAML
+
+This project is VERY VERY VERY lazy, i just wanted an extension to load my file into my toplevel and i didn't find one online, it only works if you have `ocaml` installed under the `ocaml` command. 
+Use it at your own risks if you want. The code is ugly and a plain modification of the default hello-world-template of vsc extensions.
+
+It adds a run icon on the menu AND the status bar, the command is labeled `loadInToplevel` and is keybinded with `ctrl+t` (`cmd+t` on macs ) by default. 
+
+## Prerequisites
+
+- vscode ^1.51.0
+
+## Install
+
+```sh
+npm install
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **Charlotte T.**
+
+* Twitter: [@coco33920](https://twitter.com/coco33920)
+* Github: [@coco33920](https://github.com/coco33920)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { runInContext } from 'vm';
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let run = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+	run.text = "$(debug-start) Load into toplevel"
+	run.command = "ocaml-toplevel-loader.loadIntoToplevel"
+	run.show();
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "ocaml-toplevel-loader" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+
+
+	let load = vscode.commands.registerCommand('ocaml-toplevel-loader.loadIntoToplevel', () => {
+		if (vscode.window.activeTextEditor === undefined){
+			vscode.window.showErrorMessage("Error, you must use a .ml file for this command.")
+			return;
+		}
+		let file = vscode.window.activeTextEditor?.document.fileName;
+		if (!(file?.endsWith(".ml") || file === undefined)){
+			vscode.window.showErrorMessage("Error, you must use a .ml file for this command.")
+			return;
+		}
+		let currentTerminal = vscode.window.activeTerminal;
+		if (currentTerminal === undefined){
+			let terminal = vscode.window.createTerminal('ocamlrun');
+			terminal.show()
+			terminal.sendText("ocaml");
+				setTimeout(() => {
+					if (terminal === undefined) {
+						return;
+					}
+					terminal.sendText("#use \"" + file + "\";;")}, 1000)
+			return;
+		}	
+		currentTerminal.show()
+		if (currentTerminal.name !== "ocamlrun"){
+			currentTerminal.sendText("ocaml");
+			setTimeout(() => {
+				if (currentTerminal === undefined) {
+					return;
+				}
+				currentTerminal.sendText("#use \"" + file + "\";;")}, 1000)
+			}else{
+				currentTerminal.sendText("#use \"" + file + "\";;")
+			}
+		});
+	context.subscriptions.push(load);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
